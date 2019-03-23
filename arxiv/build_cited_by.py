ec1e0356f6772faab260da4c16bd68e7b090e78a
soodoku <gsood07@gmail.com>
2019-03-23 12:31:17 -0700

    Arxiv (#6)
    * step 1 build cited by list of the articles
    
    * fix file name and location
    
    * step 2 search and collect the sentences from citing articles
    
    * add script to download KDD Cup 2003 dataset
    
    * add a sample output (from 100 articles)
    
    * fix the link to file
    
    * correct file name

---
+++ arxiv/build_cited_by.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import tarfile
+
+
+if __name__ == "__main__":
+
+    with tarfile.open("./kddcup2003/hep-th-citations.tar.gz", "r:gz") as t:
+        for m in t.getmembers():
+            f = t.extractfile(m)
+            df = pd.read_csv(f, delim_whitespace=True,
+                             header=None, names=['from', 'to'])
+            break
+
+    out = []
+    for c in df.to.unique():
+        cited = []
+        for a in df[df.to == c]['from'].unique():
+            cited.append('%07d' % a)
+        out.append(['%07d' % c, ';'.join(cited)])
+
+    xdf = pd.DataFrame(out)
+
+    xdf.columns = ['article_id', 'citing_article_ids']
+    xdf.to_csv('hep-th-cited-by.csv', index=False)
